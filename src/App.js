f2985b5a15dde85a84cdf92e3db9258b0b1fc31a
MaksGovor <maksgovruha@gmail.com>
2022-10-09 12:54:42 +0300

    Init basic SPA for lab1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import './App.css';
+import CardItem from './components/CardItem';
+import Dmytro from './assets/dmytro.jpg'
+import Maksym from './assets/maksym.jpg'
 
 function App() {
   return (
-    <div className="App">
-      Scopics
+    <div className='App'>
+       <div class='container'>
+        <div class='header'>
+          <h3 class='suptitle'>Who we are</h3>
+          <h2 class='title'>Meet our team</h2>
+          <div class='text'>
+            <p>Lab Work 1: SPA, nginx, docker</p>
+            </div>
+        </div>
+        <div class='card_container'>
+          <CardItem image={Dmytro} name={'Dmytro Uryn'} info={'Group: IP-93'}/>
+          <CardItem image={Maksym} name={'Maksym Govoruha'} info={'Group: IP-93'}/>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/CardItem/CardItem.jsx
@@ -0,0 +1,19 @@
+import './CardItem.css';
+
+const CardItem = ({image, name, info}) => {
+  return (
+    <div class='card_item'>
+       <div class='card_inner'>
+         <div class='card_image'>
+           <img src={image} alt={name}/>
+         </div>
+       </div>
+       <div class='card_info'>
+         <div class='card_name'>{name}</div>
+         <div class='card_info'>{info}</div>
+       </div>
+    </div>
+  );
+}
+
+export default CardItem;
